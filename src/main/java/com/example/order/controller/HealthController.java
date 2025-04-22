b2c1fb7bd5ca4b0844320b4c967da6c9edb56001
Thiago <thiagobeppler@hotmail.com>
2025-04-22 15:01:52 -0300

    Ajuste HealthController

--- src/main/java/com/example/order/controller/HealthController.java
+++ src/main/java/com/example/order/controller/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
-public class healthController {
+public class HealthController {
 
     @GetMapping()
     public ResponseEntity health() {
